b6420770e5230b83620d9592f9e56678e97e608c
A. David Barros <dbpiano@gmail.com>
2018-03-22 21:58:24 -0400

    Images

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -21,7 +21,7 @@ class Welcome extends CI_Controller {
 	public function index()
 	{
 		
-		//$this->myFakeFunc();
-		$this->load->view('welcome_message');
+		
+		$this->load->view('welcome_message')
 	}
 }
